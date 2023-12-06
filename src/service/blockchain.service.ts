83288eb8aeee8aa4d7354d6b755231dd712253c0
pavel <paveltimosh@gmail.com>
2023-12-06 11:05:19 +0300

    add estimation

--- src/service/blockchain.service.ts
+++ src/service/blockchain.service.ts
@@ -23,8 +23,14 @@ export class BlockchainService {
   }
 
   public async executeOrder(priceFeedUpdateData: string[] | null, account: string): Promise<string> {
+    this.logger.log(`estimating order ${account} execution...`);
+    const estimated = await this.delayedOrderContractWithSigner.estimateGas.executeOrder(account, priceFeedUpdateData, {
+      value: '1',
+    });
+
+    this.logger.log(`order ${account} execution tx estimated: ${estimated}`);
     const tx = await this.delayedOrderContractWithSigner.executeOrder(account, priceFeedUpdateData, {
-      gasLimit: ethers.utils.hexlify(BigNumber.from('2000000')),
+      gasLimit: ethers.utils.hexlify(estimated.add(estimated.mul(40).div(100))),
       gasPrice: ethers.utils.parseUnits('1.5', 'gwei'),
       value: '1',
     });
