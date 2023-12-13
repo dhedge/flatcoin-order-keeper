4bba5ed9c6ff86ecdd9f6ee864a85a7970c71820
artem <artemyoy@gmail.com>
2023-12-13 21:16:43 +0300

    add try catch to estimate error parser

--- src/service/error.handler.ts
+++ src/service/error.handler.ts
@@ -27,13 +27,18 @@ export class ErrorHandler {
 
   public getGasEstimateErrorName(gasEstimateError: string): string {
     if (!gasEstimateError) return "";
-    const jsonStartIndex = gasEstimateError.toString().indexOf("error=") + 6;
-    const jsonEndIndex = gasEstimateError.toString().indexOf(", code=");
-    if (jsonStartIndex > 0) {
-      const errorJSON = JSON.parse(gasEstimateError.toString().slice(jsonStartIndex, jsonEndIndex));
-      return this.errorInterface.parseError(errorJSON.error.data).name;
-    } else {
-      return gasEstimateError;
+    try {
+      const jsonStartIndex = gasEstimateError.toString().indexOf("error=") + 6;
+      const jsonEndIndex = gasEstimateError.toString().indexOf(", code=");
+      if (jsonStartIndex > 0) {
+        const errorJSON = JSON.parse(gasEstimateError.toString().slice(jsonStartIndex, jsonEndIndex));
+        return this.errorInterface.parseError(errorJSON.error.data).name;
+      } else {
+        return gasEstimateError;
+      }
+    } catch (error) {
+      this.logger.error("can't get gas estimate error", error);
+      return "";
     }
   }
 }
